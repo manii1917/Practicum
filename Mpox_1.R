150a68e50db5036ed2f1656a0aee64193c611b93
manii1917 <116575021+manii1917@users.noreply.github.com>
2022-10-24 12:20:05 -0400

    Add files via upload
    My First File

---
+++ Mpox_1.R
@@ -0,0 +1,26 @@
+#Install Required Libraries
+install.packages("RSocrata")
+library(RSocrata)
+install.packages("tidyverse")
+library(tidyverse)
+
+#Read MonkeyPox Data into a tibble
+monkey_pox_dt <- 
+  RSocrata::read.socrata(
+    url = "https://data.ct.gov/resource/qhtt-czu2.json"
+  ) %>% 
+  as_tibble()
+
+#Look at the Data to verify it looks okay
+print(monkey_pox_dt)
+
+#Basic plot on count of cases for each report period start
+ggplot(data = monkey_pox_dt) + 
+  geom_bar(mapping = aes(x = reportperiodstart))
+
+#Modifying the existing plot to display label and change the colors
+ggplot(data = monkey_pox_dt) + 
+  geom_bar(mapping = aes(x = reportperiodstart,fill = ..count..)) +
+  geom_text(aes(x = reportperiodstart, label = ..count..), stat = "count", vjust = 1.5, colour = "white")
+
+
